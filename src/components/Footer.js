a112f07a103a006ce8ea08287fd1fc65369b729b
David Wachlin <davidwachlin@gmail.com>
2020-01-18 15:05:07 -0500

    adds footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,15 +48,7 @@ const Footer = () => (
           <FooterContainer>
             <Fade left>
               <TextFooter fontSize={[2, 3]}>
-                <span>{`${name} Portfolio - Powered by `}</span>
-                <Link href="https://www.gatsbyjs.org/">Gatsby</Link>
-                <span> and </span>
-                <Link href="https://www.contentful.com/" mr={1}>
-                  Contentful
-                </Link>
-                <span role="img" aria-label="heart">
-                  ❤️
-                </span>
+                <span>{`${name} Portfolio`}</span>\
               </TextFooter>
             </Fade>
             <Flex>
